a4bd48107cc1e6cb60a37f58ce233f314fd79546
GDianaS <gab13ssousa@gmail.com>
2024-08-25 19:13:21 -0300

    atualização da view Consulta Animal

--- resources/views/consulta_animais/show.blade.php
+++ resources/views/consulta_animais/show.blade.php
@@ -1,19 +1,52 @@
 @extends('layout')
-
 @section('title', $animal->nome)
 @section('content')
-<p>Nome do Animal: {{$animal->nome}}</p>
+
+
+<a class="link-voltar" href="/consulta_animal">Voltar</a><br>
 
 @if(Auth::user()->isDivisa || $can_view_more_details)
-  <p>{{$animal->cpf_ou_cnpj_proprietario}}</p>
-  <p>{{$animal->dataNascimento}}</p>
-  <p>{{$animal->sexo}}</p>
-  <p>{{$animal->fase}}</p>
-  <p>{{$animal->porte}}</p>
-  <p>{{$animal->codigo_microchip}}</p>
-  <p>{{$animal->especie}}</p>
-  <p>{{$animal->hasPedigree}}</p>
+<div class="panel-body bio-graph-info">
+  <h3 class="text-center mt-2 mb-2" >Dados do Animal</h3>
+  <div class="row">
+          <div class="bio-row">
+              <p><b>Nome:</b> {{$animal->nome}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Data de Nascimento:</b> {{$animal->dataNascimento}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Sexo:</b> {{$animal->sexo}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Fase de Crescimento:</b> {{$animal->fase}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Porte:</b> {{$animal->porte}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Especie:</b> {{$animal->especie}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Pedigree:</b></p>
+              @if(($animal->hasPedigree) == 0)
+                <p>Não</p>
+              @endif
+              @if(($animal->hasPedigree) == 1)
+                <p>Sim</p>
+              @endif
+          </div>
+          <div class="bio-row">
+              <p><b>CPF/CNPJ do Proprietário:</b> {{$animal->cpf_ou_cnpj_proprietario}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Código do Microchip:</b> {{$animal->codigo_microchip}}</p>
+          </div>
+          <div class="bio-row">
+              <p><b>Telefone da Credenciada:</b> {{$animal->telefone_credenciada}}</p>
+          </div>
+
+</div>
 @endif
 
-<p>Telefone da Credenciada: {{$animal->telefone_credenciada}}</p>
 @endsection
